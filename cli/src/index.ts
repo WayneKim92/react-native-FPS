972fc92478c95cc4cb241591d49579ec569c5d64
WayneKim92 <eunchan0817@gamil.com>
2024-04-24 19:45:10 +0900

    chore: npx로 실행 가능 bin 추가

--- cli/src/index.ts
+++ cli/src/index.ts
@@ -8,15 +8,16 @@ showLogo();
 program
     .name('react-native-fps-cli')
     .description('FPS-based RN app performance measurement tool')
-    .version('0.1.0')
+    .version('0.0.1')
 
 program.command('init')
     .description('Initialize monitor-backend for react-native-fps')
-    .action(async () => init());
+    .action(init);
 
 program.command('report')
     .description('Generate fps report by period')
-    .action(async () => report());
+    .action(report);
 
 program.parse();
 
+export const cli = program;
